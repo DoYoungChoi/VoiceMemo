16ecbb27c9b64b146eddac0001e06ee093345885
dodor <choidy9298@naver.com>
2023-10-13 17:20:15 +0900

    Initial Commit

---
+++ VoiceMemo/VoiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoApp.swift
+//  VoiceMemo
+//
+//  Created by dodor on 2023/10/13.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
